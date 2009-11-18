ac96c818e6908b85834e58f260d0b1fb64252d8a
Luan Almeida <luanlmd@gmail.com>
2009-11-18 11:47:45 -0300

    Some improvements and some strings fixed

--- pytwify.py
+++ pytwify.py
@@ -8,55 +8,61 @@ config = ConfigParser.ConfigParser()
 if pynotify.init("Twitter"):
 
 	firstRun = True
+	configFile = os.getenv("HOME")+'/.pytwify.cfg'
 	
 	while True:
-	
-			config.read(os.getenv("HOME")+'/.pytwify.cfg')
-			username = config.get('auth', 'username')
-			password = config.get('auth', 'password')
-			lastId = config.getint('vars', 'lastId')
+
+			try:
+				
+				config.read(configFile)			
+				username = config.get('auth', 'username')
+				password = config.get('auth', 'password')
+				lastId = config.getint('vars', 'lastId')
 		
-			api = twitter.Api(username, password)		
+				api = twitter.Api(username, password)
 
-			if firstRun:
+				if firstRun:
 							
-				try:
-					twitts = api.GetFriendsTimeline(count = 1)
-					firstRun = False
-					if len(twitts) > 0:
-						lastId = twitts[0].id
+					try:
+						twitts = api.GetFriendsTimeline(count = 1)
+						firstRun = False
+						if len(twitts) > 0:
+							lastId = twitts[0].id
 				
-				except:
-					print "auth error =("
-					pynotify.Notification("Pytter: Error ", "Couldn't authenticate.\n Edit the file pytter.cfg").show()
-							
-			else:
+					except:
+						print "auth error =("
+						pynotify.Notification("PyTwiFy: Error ", "Couldn't authenticate.\n Edit the file "+configFile).show()
+									
+				else:
 
-				try:
+					try:
 
-					print "lastId is: " + str(lastId)
+						print "lastId is: " + str(lastId)
 				
-					twitts = api.GetFriendsTimeline(since_id = lastId)
+						twitts = api.GetFriendsTimeline(since_id = lastId)
 
-					print "twitt(s) retrieved: " + str(len(twitts))
+						print "twitt(s) retrieved: " + str(len(twitts))
 		
-					if len(twitts) > 0:
-						lastId = twitts[0].id
+						if len(twitts) > 0:
+							lastId = twitts[0].id
 
-						for i in range(len(twitts)-1, -1, -1):
-							t = twitts[i]
-							print "showing twitt from " + t.user.screen_name + " (id:" + str(t.id) + ")"
-							pynotify.Notification("Pytter: " + t.user.screen_name, t.text).show()
+							for i in range(len(twitts)-1, -1, -1):
+								t = twitts[i]
+								print "showing twitt from " + t.user.screen_name + " (id:" + str(t.id) + ")"
+								pynotify.Notification("Pytter: " + t.user.screen_name, t.text).show()
 				
-				except:
-					print "some error =("
-					pynotify.Notification("Pytter: Error ", "Some connection error ocurred...").show()
+					except:
+						print "some error =("
+						pynotify.Notification("PyTwiFy: Error ", "Some connection error ocurred...").show()
 					
-			config.set("vars","lastId",lastId)
-			config.write(open(os.getenv("HOME")+'/.pytwify.cfg','wb'))
+				config.set("vars","lastId",lastId)
+				config.write(open(configFile,'wb'))
 		
-			print "next lastId is: " + str(lastId)
-			print "sleeping..."
+				print "next lastId is: " + str(lastId)
+				print "sleeping..."
+				
+			except:
+				pynotify.Notification("PyTwiFy: Error ", "Error while reading the configuration.\n Please setup the file "+configFile).show()
 
 			sleep(60)
 		
