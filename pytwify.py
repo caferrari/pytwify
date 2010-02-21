af65f5311d546640d31dd08a26ae6da897cf6f82
Carlos André Ferrari <caferrari@gmail.com>
2010-02-21 11:54:44 -0300

    Changed to class like script and some improvements

--- pytwify.py
+++ pytwify.py
@@ -1,76 +1,76 @@
-import pynotify, twitter, os.path, os, sys
-import urllib2
+#!/usr/bin/python2.6
+# -*- coding: utf-8 -*-#
+import pynotify, twitter, os.path, os, sys, urllib2, tempfile
 from time import sleep
+from ConfigParser import ConfigParser
 
-import ConfigParser
+class pytwify:
 
-config = ConfigParser.ConfigParser()
+	def log(self, msg):
+		print msg
 
-if pynotify.init("Twitter"):
-
-	configFile = os.getenv("HOME")+'/.pytwify.cfg'
-	cachePath = '/tmp/pytwify/'
-	lastId = 0
-	
-	while True:
-
-		config.read(configFile)			
-		username = config.get('auth', 'username')
-		password = config.get('auth', 'password')
+	def saveConf(self):
+		self.log ('Saving the configfile %s' % self.configFile)
+		self.config.has_section('config') or self.config.add_section('config')
+		self.config.set('config', 'auth.username', self.username)
+		self.config.set('config', 'auth.password', self.password)
+		self.config.set('config', 'lastId', self.lastId)
 		
-		lastIdFile = cachePath + username+".last"
-
-		api = twitter.Api(username, password)
-
-		if not os.path.exists(lastIdFile):
-			if not os.path.exists(cachePath):
-				os.mkdir(cachePath)
-					
-			try:		
-				twitts = api.GetFriendsTimeline(count = 1)
-				if len(twitts) > 0:
-					lastId = twitts[0].id
-					if lastId != 0:
-						open(lastIdFile,"wb").write(str(lastId))
+		for d in ('%s/.pytwify' % self.homedir, '%s/.pytwify/cache' % self.homedir):
+			os.path.isdir(d) or os.mkdir(d)
 				
-			except:
-				print "auth error =("
-				pynotify.Notification("PyTwiFy: Error ", "Couldn't authenticate.\n Edit the file "+configFile).show()
-									
-		else:
+		self.config.write(open(self.configFile, 'wb'))
+				
+		if self.username == 'not-set':
+			self.log ('Put your login info in the config file\n\nbye bye...')
+			sys.exit(0) # Maybe a screen to enter the username and password? mayyyybe
 		
-			lastId = open(lastIdFile).read()
+	def readConf(self):
+		self.config = ConfigParser()
+		if os.path.isfile(self.configFile):
+			self.config.read(self.configFile)
+			self.username = self.config.get('config', 'auth.username')
+			self.password = self.config.get('config', 'auth.password')
+			self.lastId = self.config.get('config', 'lastId')
+			
+		if self.username == 'not-set':
+			self.saveConf()
+			
+	def __init__(self):
+		self.username = 'not-set'
+		self.password = 'not-set'
+		self.lastId = 0
 	
-			try:
-
-				print "lastId is: " + str(lastId)
-	
-				twitts = api.GetFriendsTimeline(since_id = lastId)
-
-				print "twitt(s) retrieved: " + str(len(twitts))
-
-				if len(twitts) > 0:
-					lastId = twitts[0].id
-
-					for i in range(len(twitts)-1, -1, -1):
-						t = twitts[i]
-						print "showing twitt from " + t.user.screen_name + " (id:" + str(t.id) + ")"
-					
-						uri = cachePath + "image_" + t.user.screen_name 
-						if not os.path.exists(uri):
-							open(uri,"w").write(urllib2.urlopen(t.user.profile_image_url).read())
-					
-						n = pynotify.Notification("PyTwiFy: " + t.user.screen_name, t.text, uri)
-						n.show()
-					
-					open(lastIdFile,"wb").write(str(lastId))
-						
-			except:
-				print "some error =("
-				pynotify.Notification("PyTwiFy: Error ", "Twitter is \"baleiando\".").show()
-						
-		print "next lastId is: " + str(lastId)
-		print "sleeping..."
-
+		try:
+			from win32com.shell import shellcon, shell
+			self.homedir = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0)
+		except ImportError:
+			self.homedir = os.path.expanduser("~")
+		
+		self.configFile = self.homedir + '/.pytwify/config.cfg'
+		self.cachePath =  self.homedir + '/.pytwify/cache/'
+		self.readConf()
+		self.twitter = twitter.Api(self.username, self.password)
+		pynotify.init('pytwify')
+		self.showtime()
+		
+	def showtime(self):
+		try:
+			twitts = self.twitter.GetFriendsTimeline(since_id = int(self.lastId))
+			self.log ('%d new tweets found' % len(twitts))
+			if (twitts):
+				self.lastId = twitts[0].id
+	 			self.saveConf()
+			while twitts:
+				t = twitts.pop()
+				uri = '%s/image_%s' % (self.cachePath, t.user.screen_name)
+				os.path.isfile(uri) or open(uri, 'w').write(urllib2.urlopen(t.user.profile_image_url).read())
+				pynotify.Notification('PyTwiFy: %s ' % t.user.screen_name, t.text, uri).show()
+		except:
+			self.log('Some error trying to retrive the tweets')
+			
+		self.log ('sleeping...')
 		sleep(60)
+		self.showtime()
 		
+pytwify()
